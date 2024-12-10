26633518161891340d44e96b38852488fa692a35
tneron2023 <theo.neron2023@campus-eni.fr>
2024-12-10 16:43:32 +0100

    Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import {Routes} from '@angular/router';
 import {HomeComponent} from "./components/mod5/home/home.component";
 import {AboutComponent} from "./components/mod5/about/about.component";
+import {DetailComponent} from "./components/mod5/detail/detail.component";
+import {authGuardGuard} from "./guards/auth-guard.guard";
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'home', component: HomeComponent},
-  {path: 'about-us', component: AboutComponent}
+  {path: 'about-us', component: AboutComponent, canActivate : [authGuardGuard]},
+  {path: 'detail/:id', component: DetailComponent}
+
 ];
---
+++ src/app/guards/auth-guard.guard.ts
@@ -0,0 +1,15 @@
+import {CanActivateFn, Router} from '@angular/router';
+import {inject} from "@angular/core";
+import {Mod4demo1Service} from "../services/mod4demo1.service";
+
+export const authGuardGuard: CanActivateFn = (route, state) => {
+
+  const authService = inject(Mod4demo1Service)
+  const router = inject(Router)
+  if (authService.getUser()) {
+    return true;
+  } else {
+    router.navigate(['/detail', 123], {queryParams : {message : "Mange tes morts"}})
+    return false;
+  }
+};
