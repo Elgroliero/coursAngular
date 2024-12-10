cd77ff45ae6eb6cfa37c6156a2a265cf438c755d
stropee <stropee@campus-eni.fr>
2024-12-10 15:45:33 +0100

    mod 5 - route simple

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {HomeComponent} from "./components/mod5/home/home.component";
+import {AboutComponent} from "./components/mod5/about/about.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'about-us', component: AboutComponent}
+];
