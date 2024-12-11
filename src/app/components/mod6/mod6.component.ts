7f4246abb7799c61caf5466336866ff09e914bcb
tneron2023 <theo.neron2023@campus-eni.fr>
2024-12-11 14:15:30 +0100

    Formulaire

---
+++ src/app/components/mod6/mod6.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mod6',
+  standalone: true,
+  imports: [],
+  templateUrl: './mod6.component.html',
+  styleUrl: './mod6.component.css'
+})
+export class Mod6Component {
+
+}
