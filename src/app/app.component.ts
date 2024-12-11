38e58c320ae845efbd6f26633bf712ccd9b2ea65
tneron2023 <theo.neron2023@campus-eni.fr>
2024-12-11 14:15:09 +0100

    Formulaire

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Mod3demo1Component} from "./components/mod3demo1/mod3demo1.component";
 import {Mod4demo1Component} from "./components/mod4demo1/mod4demo1.component";
+import {Mod6Component} from "./components/mod6/mod6.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, Mod3demo1Component, Mod4demo1Component],
+  imports: [RouterOutlet, Mod3demo1Component, Mod4demo1Component, Mod6Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
