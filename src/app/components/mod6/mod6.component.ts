33e49beea5791725ba5dc8e3c3a57f70ad6c5580
tneron2023 <theo.neron2023@campus-eni.fr>
2024-12-11 15:50:08 +0100

    Formulaire et données récupérées

--- src/app/components/mod6/mod6.component.ts
+++ src/app/components/mod6/mod6.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-mod6',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    NgIf
+  ],
   templateUrl: './mod6.component.html',
   styleUrl: './mod6.component.css'
 })
-export class Mod6Component {
+export class Mod6Component implements OnInit {
 
+  public form!: FormGroup
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      email: new FormControl('theo.neron@campus-eni.fr', [Validators.email, Validators.required]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+      birthdate: new FormControl(null, [Validators.required, this.dateValidator]),
+      job: new FormControl('design'),
+      newsletter: new FormControl(false),
+      knowledge: new FormControl(5, [Validators.min(2), Validators.max(10)])
+    });
+  }
+
+  public dateValidator(control: AbstractControl) {
+    console.log(control)
+
+    if (control && control.value) {
+      if (new Date(control.value) > new Date()) {
+        return {greaterThan: true}
+      }
+    }
+    return null
+  }
+
+  onSubmit() {
+//code exécuté lors de la soumission du formulaire
+    if (this.form.valid) {
+      //si c'est ok j'envoie à mon service/api
+      console.log(this.form.value)
+    } else {
+      console.log("Formulaire non valide")
+
+    }
+  }
 }
